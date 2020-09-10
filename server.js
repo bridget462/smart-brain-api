fc087ee563d91eb3004a6fee563e4a300ce34bb7
bridget462 <bridges.dev.create@gmail.com>
2020-09-10 21:44:34 +0900

    server checked with POSTMAN. API structure draft is created

--- server.js
+++ server.js
@@ -2,6 +2,23 @@ const express = require("express");
 
 const app = express();
 
+app.get("/", (req, res) => {
+  res.send("this is working");
+});
+
 app.listen(3000, () => {
   console.log("app is running on port 3000");
 });
+
+/*
+API structure:
+/ --> res = this is working
+
+signin is not PUT(update) because each time, user info should be secure with HTTP encoding
+/signin ==> POST = signin success/fail
+/register --> POST = user
+
+each user has rank
+/profile/:userId --> GET = user
+/image --> PUT = user
+*/
