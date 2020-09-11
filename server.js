294d6496f862613cf183f16318f9c7722bc064e8
bridget462 <bridges.dev.create@gmail.com>
2020-09-11 13:26:27 +0900

    register connected to front-end

--- server.js
+++ server.js
@@ -61,7 +61,7 @@ app.post("/signin", (req, res) => {
     req.body.email === database.users[0].email &&
     req.body.password === database.users[0].password
   ) {
-    res.json("success");
+    res.json(database.users[0]);
   } else {
     res.status(400).json("error logging in");
   }
@@ -76,7 +76,6 @@ app.post("/register", (req, res) => {
     id: "125",
     name: name,
     email: email,
-    password: password,
     entries: 0,
     joined: new Date(),
   });
