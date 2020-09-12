e9e989460236f36b5ab8138642e9c6accd356987
bridget462 <bridges.dev.create@gmail.com>
2020-09-12 15:30:17 +0900

    entires connected to db

--- server.js
+++ server.js
@@ -120,18 +120,15 @@ app.get("/profile/:id", (req, res) => {
 app.put("/image", (req, res) => {
   console.log("server, /image called");
   const { id } = req.body;
-  let found = false;
 
-  database.users.forEach((user) => {
-    if (user.id === id) {
-      found = true;
-      user.entries++;
-      return res.json(user.entries);
-    }
-  });
-  if (!found) {
-    res.status(400).json("user not found");
-  }
+  db("users")
+    .where("id", "=", id)
+    .increment("entries", 1)
+    .returning("entries")
+    .then((entries) => {
+      res.json(entries[0]);
+    })
+    .catch((err) => res.status(400).json("unable to get entries"));
 });
 
 // bcrypt-nodejs docs: https://www.npmjs.com/package/bcrypt-nodejs
